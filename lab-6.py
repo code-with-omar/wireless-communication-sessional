26548fdd4fc82b2b981659d871b2983264e86a02
Omar Faruk <omarfaruk65142@gmail.com>
2024-09-03 00:54:06 +0600

    Lab-11

--- lab-6.py
+++ lab-6.py
@@ -12,6 +12,6 @@ print(f"The frequency is : {The_received_frequency_of_A}")
 # case of vehicle A
 print("Question Number B")
 print('The vehicle is moving directly away from the transmitter: ')
-The_received_frequency_of_B_is = (carrier_frequency - (vehicle_speed / wavelength)) / 1e6
+The_received_frequency_of_B_is = (carrier_frequency - (vehicle_speed / waveLength)) / 1e6
 print(f'The received frequency is: {The_received_frequency_of_B_is} MHz')
 print()
